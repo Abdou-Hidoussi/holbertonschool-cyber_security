b1b86319364f1b8998aa40ef01301f965b391ca7
Abdou <hidoussiabdou5@gmail.com>
2023-05-17 17:25:53 +0100

    456 regex need update

--- Checker.py
+++ Checker.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 def del_folder(folder_name):
+    """ clear the content of a folder """
     if os.path.exists(folder_name):
         for filename in os.listdir(folder_name):
             file_path = os.path.join(folder_name, filename)
@@ -15,6 +16,7 @@ def del_folder(folder_name):
         print(f"'{folder_name}' does not exist.")
 
 def get_latest_commit_id(username, repo_name):
+    """ get the latest commit of a github repo """
     api_url = f"https://api.github.com/repos/{username}/{repo_name}/commits"
     response = requests.get(api_url)
     response_json = response.json()
@@ -23,6 +25,7 @@ def get_latest_commit_id(username, repo_name):
     return latest_commit_id
 
 def get_content(username, repo_name, file_path):
+    """ get github file by repo username and file path and returns it's content"""
     latest_commit_id = get_latest_commit_id(username, repo_name)
     api_url = f"https://api.github.com/repos/{username}/{repo_name}/contents/{file_path}?ref={latest_commit_id}"
     headers = {"Accept": "application/vnd.github.VERSION.raw"}
@@ -34,6 +37,7 @@ def get_content(username, repo_name, file_path):
     return content
 
 def save_content(content, file_path):
+    """ save provided content into a file """
     with open(file_path, 'w+') as file:
         file.write(content)
 
