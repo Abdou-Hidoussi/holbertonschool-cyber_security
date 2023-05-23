b8519b8870b28fdd0623271eba82db13db689fe6
Abdou <hidoussiabdou5@gmail.com>
2023-05-23 09:58:06 +0100

    check file

--- Check_Task.py
+++ Check_Task.py
@@ -50,3 +50,44 @@ def check_all(config_file=None,json_file=None,file_path=None):
                     output.append(response)
     config.close()
     return output
+
+def check_file(json_tasks=None, file_content=None):
+    output = []
+    requirment = True
+    for mod in json_tasks:
+        for check in json_tasks[mod]:
+            for conf in json_tasks[mod][check]:
+                module = importlib.import_module(mod)
+                response = {
+                    "check": check,
+                    "status": "",
+                    "error": "",
+                    "desc": getattr(module, check).__doc__,
+                    "arg": "",
+                    "file": "",
+                }
+                if requirment:
+                    file = conf[0]
+                    arg = conf[1]
+                    result = getattr(module, check)(file_content, arg)
+                    if not result:
+                        response["status"] = "Failed"
+                        response["error"] = ""
+                        response["arg"] = arg
+                        response["file"] = file
+                        if (len(conf) >= 3) and (not result):
+                            requirment = False
+                        output.append(response)
+                    else:
+                        response["status"] = "Success"
+                        response["error"] = ""
+                        response["arg"] = arg
+                        response["file"] = file
+                        output.append(response)
+                else:
+                    response["status"] = "Failed"
+                    response["arg"] = None
+                    response["file"] = None
+                    response["error"] = "Requirment Fail"
+                    output.append(response)
+    return output
